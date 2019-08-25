4eeb7240439ed3a9e43a3daad4c15dfbb3e132b9
Caner Güven <caanerguuven@hotmail.com>
2019-08-25 15:43:22 +0300

    Logging Api Example first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingApiExample.Controllers
+{
+    [Route("MyApi/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IConfiguration configuration;
+        private ILogger logger;
+
+        public ValuesController(IConfiguration _configuration,ILoggerFactory _loggerFactory) {
+            configuration = _configuration;
+            logger = _loggerFactory.CreateLogger("MyApi Logger");
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            logger.LogInformation("My Api was called by anyone - Hello World .This is Get Method");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            logger.LogInformation("My Api was called by anyone - Hello World .This is Get Method With Id");
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            try
+            {
+                logger.LogInformation("My Api was called by anyone - Hello World .This is Post Method");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Ouch ! An error was occured. I must inform.");
+                throw ex;
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            logger.LogInformation("My Api was called by anyone - Hello World .This is Put Method");
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            logger.LogInformation("My Api was called by anyone - Hello World .This is Delete Method. I suggest you to be careful.");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Exceptions;
+
+
+namespace LoggingApiExample
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            var elasticUri = Configuration["ElasticSearch:Uri"];
+            var logFileAddress= Path.Combine(Directory.GetCurrentDirectory(), Configuration["ElasticSearch:FileName"]);
+            Log.Logger = new LoggerConfiguration()
+                         .Enrich.WithExceptionDetails()
+                         .Enrich.WithMachineName()
+                         .MinimumLevel.Override("Microsoft",LogEventLevel.Error)
+                         .MinimumLevel.Override("System",LogEventLevel.Information)
+                         .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+                         {
+                             CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
+                             AutoRegisterTemplate = true,
+                             TemplateName = "serilog-events-template",
+                             IndexFormat = "loggingapiexample-{0:yyyy.MM.dd}"
+                         })
+                         .WriteTo.RollingFile(Path.Combine(logFileAddress, "paycell-log-{0:yyyy.MM.dd}"))
+                        .MinimumLevel.Verbose()
+                        .CreateLogger();
+
+        }
+
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            loggerFactory.AddSerilog();
+
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseHttpsRedirection();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "MyApi/{controller=Home}/{action}"
+                );
+            });
+        }
+    }
+}
